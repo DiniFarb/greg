7585a77058c32cefc23a276e375813fefe3768bf
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2023-02-17 20:20:36 +0100

    un/install scripts added

---
+++ install.ps1
@@ -0,0 +1,23 @@
+$version = "0.0.1-alpha"
+$url = "https://github.com/DiniFarb/greg/releases/download/$version/greg.zip"
+"Install greg from $url"
+$installDir = "C:\Program Files\Greg"
+"Install dir: $installDir"
+$appName = "greg.exe"
+$pathEnv = "C:\Program Files\Greg"
+try {
+    New-Item -ItemType Directory -Path $installDir -Force -ErrorAction Stop
+    Invoke-WebRequest $url -OutFile "$version.zip" -ErrorAction Stop
+    Expand-Archive -Path "$version.zip" -DestinationPath $installDir -Force -ErrorAction Stop
+    "Add $pathEnv to PATH Environment Variable"
+    $envPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
+    if ($envPath -notlike "*$pathEnv*") {
+        $newPath = "$envPath;$pathEnv"
+        [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+    }
+    "DONE! :) You can now run greg from any directory but you need to restart your terminal first"
+} catch  {
+    $ex = $_.Exception
+    Write-Error "Failed to install greg: $ex"
+    exit 1
+}
---
+++ uninstall.ps1
@@ -0,0 +1,17 @@
+"Uninstalling greg..."
+try {
+    $dirToRemove = "C:\Program Files\Greg"
+    $path = [Environment]::GetEnvironmentVariable("Path", "Machine")
+    $dirs = $path -split ";"
+    "Remove $dirToRemove from PATH Environment Variable"
+    $dirs = $dirs | Where-Object { $_ -ne $dirToRemove } -ErrorAction Stop
+    $newPath = $dirs -join ";"
+    "Remove $dirToRemove and all its contents"
+    Remove-Item -Path $dirToRemove -Recurse -Force -ErrorAction Stop
+    [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+    "Done! :) hope you enjoyed using greg!"
+} catch {
+    $ex = $_.Exception
+    Write-Error "Failed to uninstall greg: $ex"
+    exit 1
+}
