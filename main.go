1de1280ac4c2ce42236c27eb5edd75aba072117f
Dinifarb <andreas.vogt89@bluewin.ch>
2024-01-28 19:58:04 +0100

    b/a lines

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"bufio"
 	"flag"
 	"fmt"
@@ -8,27 +9,16 @@ import (
 	"strings"
 )
 
-var printAll = flag.Bool("a", false, "print all lines")
+var printBefore = flag.Int("b", 0, "print lines before the match")
+var printAfter = flag.Int("a", 0, "print lines after the match")
 
 func main() {
-
-	const (
-		defaultPath = "NONSET"
-	)
-
-	path := flag.String("p", defaultPath, "path to file")
-	flag.Parse()
 
 	if len(os.Args) < 2 {
 		exitWithHelp()
 	}
-	var lines []string
-	if *path == defaultPath {
-		lines = readStdin()
-	} else {
-		lines = readFileLines(*path)
-	}
-	search(lines, flag.Args())
+	flag.Parse()
+	search(readStdin(), flag.Args())
 	os.Exit(0)
 }
 
@@ -37,22 +27,6 @@ func exitWithHelp() {
 	os.Exit(1)
 }
 
-func readFileLines(filepath string) (lines []string) {
-	readFile, err := os.Open(filepath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	for fileScanner.Scan() {
-		lines = append(lines, fileScanner.Text())
-	}
-	if err := fileScanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading file:", err)
-	}
-	readFile.Close()
-	return lines
-}
 
 func readStdin() (lines []string) {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -69,16 +43,27 @@ func search(lines []string, searchTerms []string) {
 	atleastOneMatch := false
 	for i, line := range lines {
 		for _, term := range searchTerms {
-			if strings.Contains(line, term) {
+			//change Contains to Regex match
+			if regexp.MustCompile(term).MatchString(line) {
 				s := strings.Replace(line, term, fmt.Sprintf("\033[31m%s\033[0m", term), -1)
 				fin := fmt.Sprintf("%d: %s", i, s)
-				fmt.Println(fin)
-				atleastOneMatch = true
-			} else {
-				if *printAll {
-					fin := fmt.Sprintf("%d: %s", i, line)
-					fmt.Println(fin)
+				fmt.Println("***MATCH***")
+				if *printBefore > 0 {
+					for j := i - *printBefore; j < i; j++ {
+						if j >= 0 {
+							fmt.Println(fmt.Sprintf("%d: %s", j, lines[j]))
+						}
+					}
 				}
+				fmt.Println(fin)
+				if *printAfter > 0 {
+					for j := i + 1; j <= i+*printAfter; j++ {
+						if j < len(lines) {
+							fmt.Println(fmt.Sprintf("%d: %s", j, lines[j]))
+						}
+					}
+				}
+				atleastOneMatch = true
 			}
 		}
 	}
